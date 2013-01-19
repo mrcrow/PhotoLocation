d54f55bdbca5f490452fc5e9e8f5cc6734db320b
mrcrow <wwz.michael@gmail.com>
2013-01-19 23:56:21 +0800

    Initial Commit

---
+++ PhotoLocation/AppDelegate.h
@@ -0,0 +1,24 @@
+//
+//  AppDelegate.h
+//  PhotoLocation
+//
+//  Created by Wu Wenzhi on 13-1-19.
+//  Copyright (c) 2013年 Wu Wenzhi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@property (strong, nonatomic) UINavigationController *navigationController;
+
+@end
---
+++ PhotoLocation/MasterViewController.h
@@ -0,0 +1,22 @@
+//
+//  MasterViewController.h
+//  PhotoLocation
+//
+//  Created by Wu Wenzhi on 13-1-19.
+//  Copyright (c) 2013年 Wu Wenzhi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class DetailViewController;
+
+#import <CoreData/CoreData.h>
+
+@interface MasterViewController : UITableViewController <NSFetchedResultsControllerDelegate>
+
+@property (strong, nonatomic) DetailViewController *detailViewController;
+
+@property (strong, nonatomic) NSFetchedResultsController *fetchedResultsController;
+@property (strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+
+@end
